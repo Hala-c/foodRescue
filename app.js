7490456f64074d7f3d91502c685210ec30d77156
heba519 <askerheba2@gmail.com>
2026-05-01 20:20:52 +0300

    resolve merge conflict after stash pop

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./src/config/db");
 
-// routes
-const authRoutes = require("./src/routes/auth");
+dotenv.config();
+connectDB();
 
 const app = express();
-require("dotenv").config();
+
 // Middleware
-app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api/auth", authRoutes);
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-// Connect to MongoDB
-mongoose
-  .connect("mongodb://hala55:hala12345@ac-nlmql2u-shard-00-00.kktu0jm.mongodb.net:27017,ac-nlmql2u-shard-00-01.kktu0jm.mongodb.net:27017,ac-nlmql2u-shard-00-02.kktu0jm.mongodb.net:27017/Smart-ecommerc?ssl=true&replicaSet=atlas-100wdu-shard-0&authSource=admin&appName=Cluster0")
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+const PORT = process.env.PORT || 5000;
 
-// Start server
-const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
